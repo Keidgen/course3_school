c93a27fcae54fd2f7054c23e716b2d014a0b10a6
Aleksandr Malashenko <alex.08.1995@gmail.com>
2022-08-22 19:45:15 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/Course3SchoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3SchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course3SchoolApplication.class, args);
+    }
+
+}
