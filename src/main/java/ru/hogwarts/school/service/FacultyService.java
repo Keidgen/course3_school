3a6595ef3afb0085cbf7eebd9a671ec59dad3d8a
Aleksandr Malashenko <alex.08.1995@gmail.com>
2022-10-02 17:00:31 +0300

    Homework #9 done

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
@@ -12,6 +14,8 @@ import java.util.Objects;
 @Service
 public class FacultyService {
 
+    private final Logger logger = LoggerFactory.getLogger(FacultyService.class);
+
     private final FacultyRepository facultyRepository;
 
     public FacultyService(FacultyRepository facultyRepository) {
@@ -19,26 +23,32 @@ public class FacultyService {
     }
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Was invoked method for create faculty");
         return facultyRepository.save(faculty);
     }
 
     public Faculty findFaculty(long id) {
+        logger.info("Was invoked method for find faculty");
         return facultyRepository.findById(id).orElseThrow();
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        logger.info("Was invoked method for edit faculty");
         return facultyRepository.save(faculty);
     }
 
     public void deleteFaculty(long id){
+        logger.info("Was invoked method for delete faculty");
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> findByColor(String color) {
+        logger.info("Was invoked method for find faculty by color");
         return facultyRepository.findByColor(color);
     }
 
     public Collection<Faculty> findFacultiesByNameOrColor(String name, String color) {
+        logger.info("Was invoked method for find faculty by name and color");
         return facultyRepository.findFacultiesByNameIgnoreCaseOrColorIgnoreCase(name, color);
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.StudentRepository;
@@ -7,6 +9,9 @@ import java.util.Collection;
 
 @Service
 public class StudentService {
+
+    private final Logger logger = LoggerFactory.getLogger(StudentService.class);
+
     private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
@@ -14,42 +19,54 @@ public class StudentService {
     }
 
     public Student createStudent(Student student) {
+        logger.info("Was invoked method for create student");
+        logger.debug("We added student {}", student);
         return studentRepository.save(student);
     }
 
     public Student findStudent(long id) {
+        logger.info("Was invoked method for find student");
         return studentRepository.findById(id).orElseThrow();
     }
 
     public Student editStudent(Student student) {
+        logger.info("Was invoked method for edit student");
         return studentRepository.save(student);
     }
 
     public void deleteStudent(long id){
+        logger.info("Was invoked method for delete student");
+        logger.debug("We deleting student with id = {}", id);
         studentRepository.deleteById(id);
     }
 
     public Collection<Student> findByAge(int age) {
+        logger.info("Was invoked method for find student by age");
         return studentRepository.findByAge(age);
     }
 
     public Collection<Student> findByAgeBetween(int ageFrom, int ageTo) {
+        logger.info("Was invoked method for find students ages between {} and {}", ageFrom, ageTo);
         return studentRepository.findByAgeBetween(ageFrom, ageTo);
     }
 
     public Collection<Student> findAll() {
+        logger.info("Was invoked method for find all students");
         return studentRepository.findAll();
     }
 
     public Integer getCountStudents() {
+        logger.info("Was invoked method for get count students");
         return studentRepository.getCountStudents();
     }
 
     public Double getAverageAgeStudents() {
+        logger.info("Was invoked method for get average age students");
         return studentRepository.getAverageAgeStudents();
     }
 
     public Collection<Student> getLastStudents(Integer count) {
+        logger.info("Was invoked method for get {} last students", count);
         return studentRepository.getLastStudents(count);
     }
 }
